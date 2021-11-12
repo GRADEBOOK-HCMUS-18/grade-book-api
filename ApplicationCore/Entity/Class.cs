cf2804f071ce542dc871241e52d3b26cc64a678e
vitconkue <voan0935995700@gmail.com>
2021-11-12 14:36:55 +0700

    add cors and invite link for both teacher and student

--- ApplicationCore/Entity/Class.cs
+++ ApplicationCore/Entity/Class.cs
@@ -9,7 +9,8 @@ namespace ApplicationCore.Entity
         public DateTime StartDate { get; set; }
         public string Room { get; set; }
         public string Description { get; set; }
-        public string InviteString { get; set; }
+        public string InviteStringTeacher { get; set; }
+        public string InviteStringStudent { get; set; }
         public User MainTeacher { get; set; }
         public IList<ClassStudents> ClassStudents { get; set; }
         public IList<ClassTeachers> ClassTeachers { get; set; }
--- grade-book-api/Startup.cs
+++ grade-book-api/Startup.cs
@@ -82,6 +82,11 @@ namespace grade_book_api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 
             app.UseAuthorization();
 
