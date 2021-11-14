bfa9f090589c783a5888adfda012f8866dc76ef9
vitconkue <voan0935995700@gmail.com>
2021-11-14 09:31:08 +0700

    add migration: invitation link, default picture

---
+++ Infrastructure/Migrations/20211114022615_add-default-picture-hex.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class adddefaultpicturehex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "InviteString",
+                table: "Classes",
+                newName: "InviteStringTeacher");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DefaultProfilePictureHex",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InviteStringStudent",
+                table: "Classes",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DefaultProfilePictureHex",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "InviteStringStudent",
+                table: "Classes");
+
+            migrationBuilder.RenameColumn(
+                name: "InviteStringTeacher",
+                table: "Classes",
+                newName: "InviteString");
+        }
+    }
+}
