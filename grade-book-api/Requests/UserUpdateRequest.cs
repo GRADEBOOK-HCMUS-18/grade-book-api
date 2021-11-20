cab932da79cbe4eb39d7f4598a0a59d98dc7923d
vitconkue <voan0935995700@gmail.com>
2021-11-20 14:05:59 +0700

    remove password max length constraint

--- grade-book-api/Requests/UserUpdateRequest.cs
+++ grade-book-api/Requests/UserUpdateRequest.cs
@@ -7,7 +7,6 @@ namespace grade_book_api.Requests
         [EmailAddress] public string Email { get; set; }
 
         [Required]
-        [MaxLength(12)]
         [MinLength(6)]
         public string Password { get; set; }
 
