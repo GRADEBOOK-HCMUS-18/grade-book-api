d5b386ebba150c6f6603b6b399f6f5e895fb72e6
vitconkue <voan0935995700@gmail.com>
2021-11-21 10:09:17 +0700

    remove requirement field in update user

--- grade-book-api/Requests/UserUpdateRequest.cs
+++ grade-book-api/Requests/UserUpdateRequest.cs
@@ -6,7 +6,7 @@ namespace grade_book_api.Requests
     {
         [EmailAddress] public string Email { get; set; }
 
-        [Required] [MinLength(6)] public string Password { get; set; }
+         [MinLength(6)] public string Password { get; set; }
 
         [MaxLength(32)] public string FirstName { get; set; }
 
