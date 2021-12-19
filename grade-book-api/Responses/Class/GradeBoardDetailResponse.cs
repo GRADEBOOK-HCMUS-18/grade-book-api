a446eedb81ba6bf3e41181a7551da79e186124af
vitconkue <voan0935995700@gmail.com>
2021-12-19 18:46:21 +0700

    fix typo

--- grade-book-api/Responses/Class/GradeBoardDetailResponse.cs
+++ grade-book-api/Responses/Class/GradeBoardDetailResponse.cs
@@ -7,7 +7,7 @@ namespace grade_book_api.Responses.Class
 {
     public class ShortStudentGradeResponse
     {
-        public int AssigmentId { get; set; }
+        public int AssignmentId { get; set; }
         public string AssignmentName { get; set; }
         public int AssignmentWeight { get; set; }
         public int? StudentPoint { get; set; }
@@ -28,7 +28,7 @@ namespace grade_book_api.Responses.Class
                 var toAdd = new ShortStudentGradeResponse();
                 var sGrades = assignment.StudentAssignmentGrades.ToList();
 
-                toAdd.AssigmentId = assignment.Id;
+                toAdd.AssignmentId = assignment.Id;
                 toAdd.AssignmentName = assignment.Name;
                 toAdd.AssignmentWeight = assignment.Weight;
                 if (studentRecord is not null)
