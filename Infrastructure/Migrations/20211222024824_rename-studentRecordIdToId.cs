971f6720c901341783ab0e1eb75060af9335021a
vitconkue <voan0935995700@gmail.com>
2021-12-22 10:25:07 +0700

    rename studentGradeRecordId to Id to match repository interface

---
+++ Infrastructure/Migrations/20211222024824_rename-studentRecordIdToId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class renamestudentRecordIdToId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StudentAssignmentGradeId",
+                table: "StudentAssignmentGrades",
+                newName: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "StudentAssignmentGrades",
+                newName: "StudentAssignmentGradeId");
+        }
+    }
+}
